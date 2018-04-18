ab01825a5eaa105fd375d6ecc12fe1f0e1643dee
Bejoy Alias <bejoy.alias@in.ibm.com>
2018-04-18 20:03:04 +0530

    Site moved to bejoycalias.in

--- sjosc2/js/fontresize.js
+++
@@ -1,8 +0,0 @@
-var els = document.getElementsByClassName("back-light"),
-refWidth = els[0].clientWidth,
-refFontSize = parseFloat(window.getComputedStyle(els[0],null).getPropertyValue("font-size")),
-i = 1;
-while(!!els[i]){
-  els[i].style.fontSize = refFontSize * refWidth / els[i].clientWidth + "px";
-  i++;
-}
